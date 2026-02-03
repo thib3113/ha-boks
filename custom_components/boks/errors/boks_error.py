479bfa6f374c42f1500bc75eed2a8213e5a502a6
thibaut severac <thib3113@gmail.com>
2026-02-03 21:40:00 +0100

    fix tests

--- custom_components/boks/errors/boks_error.py
+++ custom_components/boks/errors/boks_error.py
@@ -8,6 +8,7 @@ class BoksError(HomeAssistantError):
     """Base class for Boks errors."""
     def __init__(self, translation_key: str, translation_placeholders: dict[str, str] | None = None):
         super().__init__(
+            translation_key,
             translation_domain=DOMAIN,
             translation_key=translation_key,
             translation_placeholders=translation_placeholders
